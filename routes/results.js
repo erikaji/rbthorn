55df68ba0d469f5fb80e2cb1b84788b40a134310
erikaji <erikaji@stanford.edu>
2014-02-05 06:34:04 +0000

    results page and routes done

--- routes/results.js
+++ routes/results.js
@@ -1,13 +1,12 @@
 exports.viewResults = function(req, res) {
-	console.log("The result is: " + req.query);
 	var list = [];
 	for (var key in req.query) {
 		list.push(key);
-		console.log("The results name is: " + key);
 	}
 	var last = list.pop();
 	res.render('results', {
 	    'resultsName': list,
-	    'last': last
+	    'last': last,
+	    'query': req.query
 	});
 }
