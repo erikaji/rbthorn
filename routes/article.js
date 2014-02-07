a42621827f6ed64bcf2a977f515970e60203a392
Ted <tedkim@stanford.edu>
2014-02-07 04:09:35 +0000

    upgraded article page to deal with simulated db

--- routes/article.js
+++ routes/article.js
@@ -8,18 +8,18 @@ exports.view = function(req, res){
 
 	// TODO: Grab article using id
 	// Using placeholder article for now
-	var title = 'Smashing Hit';
-	var headline = 'Super Bowl draws 111.5M viewers';
-	var contents = [
-		'NEW YORK -- For the fourth time in five years, the Super Bowl has set a record for the most-watched television event in U.S. history, drawing 111.5 million viewers even though the Seattle Seahawks\' 43-8 victory over the Denver Broncos wasn\'t really competitive.',
-		'The ratings record is further evidence of how live events are becoming dependable and valuable properties for broadcast television at a time the audience is fragmenting and ratings for regular entertainment shows continue to fall.',
-		'"Big-event television is a great way for people to have a communal event, to talk about it socially and to talk about it as a group," said Bill Wanger, executive vice president for programming and research at Fox Sports. "You see that in the Super Bowl numbers of the past four or five years. They\'ve just gone up to a different level."',
-	];
+	// Simulating db here - will replace with proper db later
+	var db_data = require("../data.json");
+
+	var article;
+	for(var i=0; i < db_data.articles.length; i++) {
+		console.log('checking for ' + db_data.articles[i].id);
+    if (db_data.articles[i].id == id) {
+    	article = db_data.articles[i];
+    }
+  }
 
 	res.render('article', {
-		'id': id,
-		'title': title,
-		'headline': headline,
-		'contents': contents,	
+		'article': article,
 	});
 };
--- routes/results.js
+++ routes/results.js
@@ -4,11 +4,11 @@ exports.viewResults = function(req, res) {
 
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
-	
+
 	res.render('results', {
 	    'resultsName': tags,
 	    'last': last,
 	    'query': req.query,
-	    'articles': db_data['articles'],
+	    'articles': db_data.articles,
 	});
 }
