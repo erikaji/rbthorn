783c353848e37f6771c78b58f17de92d9e2be3cd
Ted <tedkim@stanford.edu>
2014-02-07 05:27:55 +0000

    Updated choosemood tags to be more sensical, and upgraded choosemood to use db.  Also added feature to pass in a selected tag mood and linked from results page.

--- routes/choosemood.js
+++ routes/choosemood.js
@@ -1,40 +1,10 @@
 exports.view = function(req, res){
-	 res.render('choosemood', {}
-		//{
-	//     'projects': [
-	//       { 'name': 'Waiting in Line',
-	//         'image': 'lorempixel.people.1.jpeg',
-	//         'id': 'project1'
-	//       },
-	//       { 'name': 'Needfinding',
-	//         'image': 'lorempixel.city.1.jpeg',
-	//         'id': 'project2'
-	//       },
-	//       { 'name': 'Prototyping',
-	//         'image': 'lorempixel.technics.1.jpeg',
-	//         'id': 'project3'
-	//       },
-	//       { 'name': 'Heuristic Evaluation',
-	//         'image': 'lorempixel.abstract.1.jpeg',
-	//         'id': 'project4'
-	//       },
-	//       { 'name': 'Visualization',
-	//         'image': 'lorempixel.abstract.8.jpeg',
-	//         'id': 'project5'
-	//       },
-	//       { 'name': 'Social design',
-	//         'image': 'lorempixel.people.2.jpeg',
-	//         'id': 'project6'
-	//       },
-	//       { 'name': 'Gestural interaction',
-	//         'image': 'lorempixel.technics.2.jpeg',
-	//         'id': 'project7'
-	//       },
-	//       { 'name': 'Design tools',
-	//         'image': 'lorempixel.city.2.jpeg',
-	//         'id': 'project8'
-	//       }
-	// 	]  
-	// }
-	);
+	var tag = req.query.tag;
+	// Simulating db here - will replace with proper db later
+	var db_data = require("../data.json");
+
+	res.render('choosemood', {
+		'tags': db_data.tags,
+		'selected_tag': tag
+	});
 };
