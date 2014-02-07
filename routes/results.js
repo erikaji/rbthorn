a0c51b7d4c1c5e3008c0a6b53094711f5c1be069
Ted <tedkim@stanford.edu>
2014-02-07 03:11:22 +0000

    added mini db to simulate articles and updated results to take it in

--- routes/results.js
+++ routes/results.js
@@ -1,9 +1,14 @@
 exports.viewResults = function(req, res) {
 	var tags = req.query.tags;
 	var last = tags.pop();
+
+	// Simulating db here - will replace with proper db later
+	var db_data = require("../data.json");
+	
 	res.render('results', {
 	    'resultsName': tags,
 	    'last': last,
-	    'query': req.query
+	    'query': req.query,
+	    'articles': db_data['articles'],
 	});
 }
