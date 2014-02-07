790a863daf1891c8602a5202ed4b3e6344ddc742
Ted <tedkim@stanford.edu>
2014-02-07 04:33:48 +0000

    upgraded article page with back and link to actual article

--- routes/article.js
+++ routes/article.js
@@ -6,14 +6,11 @@
 exports.view = function(req, res){
 	var id = req.params.id;
 
-	// TODO: Grab article using id
-	// Using placeholder article for now
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
 
 	var article;
 	for(var i=0; i < db_data.articles.length; i++) {
-		console.log('checking for ' + db_data.articles[i].id);
     if (db_data.articles[i].id == id) {
     	article = db_data.articles[i];
     }
@@ -21,5 +18,6 @@ exports.view = function(req, res){
 
 	res.render('article', {
 		'article': article,
+		'referer': req.headers.referer
 	});
 };
