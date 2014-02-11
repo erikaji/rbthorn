83414954217956fe6467b6b3e58fe8e5b9793ce4
ted <ted@wedding-spot.com>
2014-02-10 23:09:17 -0800

    Updated to base tags off of actual article moods

--- routes/choosemood.js
+++ routes/choosemood.js
@@ -2,9 +2,17 @@ exports.view = function(req, res){
 	var tag = req.query.tag;
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
+	var tags = {};
 
+	// Go through all articles to see which moods exist
+	for (var i = 0; i < db_data.articles.length; i++) {
+		var article = db_data.articles[i];
+		for (var j = 0; j < article.moods.length; j++) {
+			tags[article.moods[j]] = true;
+		}
+  }
 	res.render('choosemood', {
-		'tags': db_data.tags,
+		'tags': tags,
 		'articles': db_data.articles,
 		'selected_tag': tag
 	});
