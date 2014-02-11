687af7a486a175443fa4de59f3e1da1767030047
ted <ted@wedding-spot.com>
2014-02-11 14:29:32 -0800

    adding underscore library and updates to results processing

--- routes/results.js
+++ routes/results.js
@@ -1,14 +1,31 @@
+var _ = require('../underscore-min.js');
+
 exports.viewResults = function(req, res) {
 	var tags = req.query.tags;
-	var last = tags.pop();
 
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
+	var articles = [];
+
+	// Go through all articles to set which articles to show
+	for (var i = 0; i < db_data.articles.length; i++) {
+		var article = db_data.articles[i];
+		var shouldShow = true;
+		for (var j = 0; j < req.query.tags.length; j++) {
+			if (!_.contains(article.moods, req.query.tags[j])) {
+				shouldShow = false;
+				break;
+			}
+		}
+
+		if (shouldShow) {
+			articles.push(article);
+		}
+  }
 
 	res.render('results', {
 	    'resultsName': tags,
-	    'last': last,
 	    'query': req.query,
-	    'articles': db_data.articles,
+	    'articles': articles,
 	});
 }
