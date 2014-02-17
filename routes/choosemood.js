8463153a1f339fcbd8f98ac832f9c34eadd5b4a6
Yu Zhai <jacky.zhai@gmail.com>
2014-02-17 14:25:45 -0800

    change color to mood tags

--- routes/choosemood.js
+++ routes/choosemood.js
@@ -4,15 +4,34 @@ exports.view = function(req, res){
 	var tag = req.query.tag;
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
+	var moodColors = require("../moodcolor.json");
 	var tags = {};
 
 	// Go through all articles to see which moods exist
 	for (var i = 0; i < db_data.articles.length; i++) {
 		var article = db_data.articles[i];
 		for (var j = 0; j < article.moods.length; j++) {
-			tags[article.moods[j]] = true;
+			tags[article.moods[j]] = '';
 		}
-  }
+
+  	}
+  	// Look up the colors
+  	for (var mood in tags){
+			var colorclass = moodColors[mood] ? "btn-" + moodColors[mood] : '';
+			switch (colorclass){
+				case "btn-orange": 
+				colorclass = "btn-warning";
+				break;
+				case "btn-red":
+				colorclass = "btn-danger";
+				break;
+				case "btn-blue":
+				colorclass = "btn-primary";
+				break;
+			}
+			tags[mood] = colorclass;
+	}
+	console.log(tags);
 	res.render('choosemood', {
 		'tags': tags,
 		'articles': db_data.articles,
