04858c91e1633e1077fed40a5daf2fbee3f56bc7
ted <ted@wedding-spot.com>
2014-02-19 04:34:10 -0800

    added colors for each mood and connected to choosemood page

--- routes/choosemood.js
+++ routes/choosemood.js
@@ -4,26 +4,60 @@ exports.view = function(req, res){
 	var tag = req.query.tag;
 	// Simulating db here - will replace with proper db later
 	var db_data = require("../data.json");
-	var moodColors = require("../moodcolor.json");
-	var tags = {};
+	var mood_colors = db_data.mood_colors;
+	var mood_rank = [];
 
-	// Go through all articles to see which moods exist
+	// Go through all articles and count up the moods
 	for (var i = 0; i < db_data.articles.length; i++) {
 		var article = db_data.articles[i];
 		for (var j = 0; j < article.moods.length; j++) {
-			tags[article.moods[j]] = '';
+			if (article.moods[j] in mood_rank) {
+				mood_rank[article.moods[j]]++;
+			} else {
+				mood_rank[article.moods[j]] = 0;
+			}
+			mood_rank[article.moods[j]]++;
 		}
+  }
 
-  	}
-  	// Look up the colors
-  	for (var mood in tags){
-			var colorclass = moodColors[mood] ? "btn-" + moodColors[mood] : '';
+  /* 
+  Algorithm we'll use for mood order is:
+  Tier 1: For > 3 articles, sorted by # of articles, highest first
+  Tier 2: For <= 3 articles, alphaetical order
+	*/
+	var tier_one_moods = [];
+	var tier_two_moods = [];
 
-			tags[mood] = colorclass;
+	// First, we need to populate into an array form like: 
+	// [ {mood: <mood>, rank: <rank>, color: <color>}, ..]
+	for (var mood in mood_rank) {
+		if (mood_rank[mood] <= 3) {
+			tier_two_moods.push({
+				'mood': mood,
+				'rank': mood_rank[mood],
+				'color': mood_colors[mood]
+			});
+		} else {
+			tier_one_moods.push({
+				'mood': mood,
+				'rank': mood_rank[mood],
+				'color': mood_colors[mood]
+			});
+		}
 	}
-	console.log(tags);
+
+	// Sort Tier 1 by rank first, then by alphetical mood
+	tier_one_moods = tier_one_moods.sort(function (a, b) {
+	    return (a.rank == b.rank ? a.mood.localeCompare( b.mood ) : b.rank - a.rank);
+	});
+
+	// Simply sort Tier 2 by alphetical mood
+	tier_two_moods = tier_two_moods.sort(function (a, b) {
+	    return a.mood.localeCompare( b.mood );
+	});
+
 	res.render('choosemood', {
-		'tags': tags,
+		'tags': tier_one_moods.concat(tier_two_moods),
 		'articles': db_data.articles,
 		'selected_tag': tag
 	});
